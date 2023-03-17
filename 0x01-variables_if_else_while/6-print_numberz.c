c38eb6e55b8d45bdf42a382665835f9aedbd6bb0
amasin76 <amasin76@gmail.com>
2023-03-17 15:36:26 +0100

    print [0-9] only with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+int zero_in_ascii = 48;
+const int NINE_IN_ASCII = 57;
+
+while (zero_in_ascii <= NINE_IN_ASCII)
+{
+putchar(zero_in_ascii);
+zero_in_ascii++;
+}
+
+putchar('\n');
+
+return (0);
+}
