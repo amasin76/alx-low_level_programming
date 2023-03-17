c1d867c0e44ce843f6300d17d929c35736bffbe3
amasin76 <amasin76@gmail.com>
2023-03-17 15:14:01 +0100

    print [a-z] & skip 'e' 'q'

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+char ch = 'a';
+
+while (ch <= 'z')
+{
+if (ch == 'e' || ch == 'q')
+{
+ch++;
+continue;
+}
+putchar(ch);
+ch++;
+}
+
+putchar('\n');
+
+return (0);
+}
