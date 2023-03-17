41a8a791edee3f07f22dfcd6e82cfb660960e071
amasin76 <amasin76@gmail.com>
2023-03-17 16:16:30 +0100

    Fix EOF

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,12 +11,14 @@ int main(void)
 int zero_in_ascii = 48;
 const int NINE_IN_ASCII = 57;
 
-while (zero_in_ascii <= NINE_IN_ASCII)
+for (; zero_in_ascii <= NINE_IN_ASCII; zero_in_ascii++)
 {
+
 putchar(zero_in_ascii);
+if (zero_in_ascii == 57)
+break;
 putchar(',');
 putchar(' ');
-zero_in_ascii++;
 }
 
 putchar('\n');
