614db837b3049fb559cb878d61354d913dfe5f01
amasin76 <amasin76@gmail.com>
2023-03-17 14:48:21 +0100

    Print [a-z] with n with only putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+char ch = 'a';
+
+while (ch <= 'z')
+{
+putchar(ch);
+putchar('\n');
+ch++;
+}
+
+return (0);
+}
