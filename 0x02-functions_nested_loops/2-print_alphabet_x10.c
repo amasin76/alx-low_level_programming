91c095552ff02b9a1893050f68bcd260db0f1de0
amasin76 <amasin76@gmail.com>
2023-03-21 11:50:19 +0000

    fix loop

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,17 +7,17 @@
 
 void print_alphabet_x10(void)
 {
-	char c;
+	int i = 0;
 
-	c = 'a';
-
-	for (int i = 0; i < 10; i++)
+	while (i < 10)
 	{
+		char c = 'a';
 		while (c <= 'z')
 		{
 			_putchar(c);
 			c++;
 		}
 		_putchar('\n');
+		i++;
 	}
 }
