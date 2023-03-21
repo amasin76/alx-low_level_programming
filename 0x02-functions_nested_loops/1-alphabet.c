682ca0a425258c8d91d3a5e1d6531b7fb0ab3ac5
amasin76 <amasin76@gmail.com>
2023-03-21 11:06:23 +0000

    print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - print "_putchar"
+ * description - prints _putchar followed by a new line
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
