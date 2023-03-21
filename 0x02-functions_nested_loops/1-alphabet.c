f7c18ad11691d18641f7ed444b6b255de5776974
amasin76 <amasin76@gmail.com>
2023-03-21 11:34:13 +0000

    Fix header

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 
 /**
  * print_alphabet - lowercase alphabets
- * Return: Always 0 (Success)
+ * Return: viod
  */
 
 void print_alphabet(void)
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,5 @@
+#ifndef MAIN
+#define MAIN
 int _putchar(char c);
+void print_alphabet(void);
+#endif
