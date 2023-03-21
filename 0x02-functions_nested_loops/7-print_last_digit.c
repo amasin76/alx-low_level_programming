554c18b42d95d1d09ea3791f851d8d0ac4f69502
amasin76 <amasin76@gmail.com>
2023-03-21 14:16:22 +0000

    Fix print digits

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,19 @@
 #include "main.h"
+/**
+ * print_last_digit - self explian
+ * @num: int num should be trim
+ * Return: int digit
+ */
 
 int print_last_digit(int num)
 {
+	int last_digit = 1;
+
 	if (num < 0)
 		num *= -1;
 
-	return num % 10;
+	last_digit = num % 10;
+	_putchar(last_digit + 48);
+
+	return (last_digit);
 }
