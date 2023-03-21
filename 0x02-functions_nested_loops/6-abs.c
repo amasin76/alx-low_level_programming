ac17c6dec9eb9beb61b2a39b0972e21fe94d5410
amasin76 <amasin76@gmail.com>
2023-03-21 13:34:16 +0000

    abs value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - prints absolute value of a number
+ * @num: int num should be absolute
+ * Return: int positive
+ */
+
+int _abs(num)
+{
+	return (abs(num));
+}
