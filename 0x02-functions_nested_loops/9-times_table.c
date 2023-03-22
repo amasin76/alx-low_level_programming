e9968a9269698f4219e1a9b96a47252aa29900f2
amasin76 <amasin76@gmail.com>
2023-03-22 02:52:47 +0000

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+
+/**
+ * times_table - Prints the 9 times table
+ */
+void times_table(void)
+{
+	int num, pos, result;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+		for (pos = 1; pos <= 9; pos++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			result = num * pos;
+			if (result <= 9)
+				_putchar(' ');
+			else
+				_putchar((result / 10) + '0');
+			_putchar((result % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
