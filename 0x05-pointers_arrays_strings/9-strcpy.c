0998b70cb6205af07244f088724d071b6b4db3bb
amasin76 <amasin76@gmail.com>
2023-03-27 15:13:23 +0000

    Fix betty style

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,19 +2,14 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * _strcpy - copies a string from src to dest
+ * @dest: the string to copy to
+ * @src: the string to copy from
+ * Return: dest the string the has src content
+ */
 char *_strcpy(char *dest, char *src)
 {
 	strcpy(dest, src);
 	return (dest);
-}
-
-int main(void)
-{
-	char s1[98];
-	char *ptr;
-
-	ptr = _strcpy(s1, "First, solve the problem. Then, write the code\n");
-	printf("%s", s1);
-	printf("%s", ptr);
-	return (0);
 }
