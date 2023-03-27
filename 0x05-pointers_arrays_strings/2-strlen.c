c41d6327707acc53bfe3ec113140f283a194860a
amasin76 <amasin76@gmail.com>
2023-03-27 12:06:48 +0000

    func return length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - length of a given string
+ * @s: the string
+ * Return: the length of given string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
