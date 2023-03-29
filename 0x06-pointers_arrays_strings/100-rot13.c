42c4afdfc452385aadf77a8d69312bb84066b69b
amasin76 <amasin76@gmail.com>
2023-03-29 20:43:55 +0000

    task asking only one if statement

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,18 +7,31 @@
  **/
 char *rot13(char *str)
 {
-	int len = 0, i = 0;
+	int i = 0, j;
 
-	while (str[len])
-		len++;
+	char *raw = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *r13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (i = 0; i < len; i++)
+	for (; str[i]; i++)
 	{
-		if ((str[i] >= 'a' && str[i] < 'n') || (str[i] >= 'A' && str[i] < 'N'))
-			str[i] += 13;
-		else if ((str[i] > 'm' && str[i] < 'z') || (str[i] > 'M' && str[i] < 'Z'))
-			str[i] -= 13;
+		j = 0;
+		for (; raw[j]; j++)
+			if (raw[j] == str[i])
+			{
+				str[i] = r13[j];
+				break;
+			}
 	}
-
 	return (str);
 }
+
+/**
+ * two if's
+ * for (i = 0; i < len; i++)
+ *	{
+ *		if ((str[i] >= 'a' && str[i] < 'n') || (str[i] >= 'A' && str[i] < 'N'))
+ *			str[i] += 13;
+ *		else if ((str[i] > 'm' && str[i] < 'z') || (str[i] > 'M' && str[i] < 'Z'))
+ *			str[i] -= 13;
+ *	}
+ */
