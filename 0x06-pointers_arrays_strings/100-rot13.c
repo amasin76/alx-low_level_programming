1c6b6dc6ef3f78aa1e402b9f2b6670a39b2065d1
amasin76 <amasin76@gmail.com>
2023-03-29 20:49:25 +0000

    guess what: betty style

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -24,14 +24,3 @@ char *rot13(char *str)
 	}
 	return (str);
 }
-
-/**
- * two if's
- * for (i = 0; i < len; i++)
- *	{
- *		if ((str[i] >= 'a' && str[i] < 'n') || (str[i] >= 'A' && str[i] < 'N'))
- *			str[i] += 13;
- *		else if ((str[i] > 'm' && str[i] < 'z') || (str[i] > 'M' && str[i] < 'Z'))
- *			str[i] -= 13;
- *	}
- */
