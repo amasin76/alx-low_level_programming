b0cede63102c3d608d3c7efe67235f7021a49f64
amasin76 <amasin76@gmail.com>
2023-04-03 20:08:01 +0000

    Fix betty style

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,10 +9,10 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int i = 0;
+	unsigned int i = 0;
 
-    for (; i < n; i++)
-        s[i] = b;
+	for (; i < n; i++)
+		s[i] = b;
 
-    return (s);
+	return (s);
 }
