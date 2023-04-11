861d1ec0648ef5d141f7153d784193ab885b39c8
amasin76 <amasin76@gmail.com>
2023-04-11 23:07:49 +0000

    Fix: plenty errors 😫

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,13 +10,24 @@
  */
 char *argstostr(int ac, char **av)
 {
-	char *str;
-	int i = 0;
+	char *str = NULL;
+	int i, j, len = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	for (; i < ac; i++)
+	for (i = 0; i < ac; i++)
+		for (j = 0; av[i][j]; j++)
+			len++;
+
+	/* for newline characters will added to the final string */
+	len += ac;
+
+	str = malloc(sizeof(char) * len);
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
 	{
 		str = str_concat(str, av[i]);
 		str = str_concat(str, "\n");
