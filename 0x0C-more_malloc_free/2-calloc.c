24610a989a6df7da8afd21ad079f4b81f871635e
amasin76 <amasin76@gmail.com>
2023-04-13 08:47:38 +0000

    Initialize 'i'

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,7 +11,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *arr;
-	unsigned int i;
+	unsigned int i = 0;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -20,7 +20,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (arr == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmemb * size; i++)
+	for (; i < nmemb * size; i++)
 		arr[i] = 0;
 
 	return (arr);
