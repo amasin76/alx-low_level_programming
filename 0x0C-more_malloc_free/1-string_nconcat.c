bdd693a081231a608cdf70d3f0bd9916c9f88009
amasin76 <amasin76@gmail.com>
2023-04-13 11:58:57 +0000

    grap all the funcs

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,9 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+
+int _strlen(char *s);
+char *_strncpy(char *dest, char *src, int n);
+char *str_concat(char *s1, char *s2);
 
 /**
  * string_nconcat - concatenates two strings up to n bytes of s2.
@@ -27,4 +30,73 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	_strncpy(s2_slice, s2, n);
 
 	return (str_concat(s1, s2_slice));
+}
+
+/**
+ * _strlen - length of a given string
+ * @s: the string
+ * Return: the length of given string
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	while (s[len])
+		len++;
+
+	return (len);
+}
+
+/**
+ * _strncpy - copies n bytes from src to dest
+ * @dest: the destination string
+ * @src: the source string
+ * @n: the number of bytes
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	for (; src[i] && i < n; i++)
+		dest[i] = src[i];
+
+	for (; dest[i] && i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string to be concatenated
+ * @s2: second string to be concatenated
+ * Return: pointer to the new string, or NULL if fails
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *new_str;
+	int i = 0, j = 0, len_s1 = 0, len_s2 = 0;
+
+	(s1 == NULL) && (s1 = "");
+	(s2 == NULL) && (s2 = "");
+
+	while (s1[len_s1])
+		len_s1++;
+	while (s2[len_s2])
+		len_s2++;
+
+	new_str = malloc(sizeof(char) * (len_s1 + len_s2 + 1));
+
+	if (new_str == NULL)
+		return (NULL);
+
+	for (; i < len_s1; i++)
+		new_str[i] = s1[i];
+	for (; j < len_s2; j++, i++)
+		new_str[i] = s2[j];
+
+	new_str[i] = '\0';
+
+	return (new_str);
 }
