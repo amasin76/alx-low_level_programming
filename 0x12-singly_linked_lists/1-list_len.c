67acb6c157a6069a3a1b3dc4d74e8693ed088245
amasin76 <amasin76@gmail.com>
2023-04-27 16:28:11 +0000

    Del: main func from the file

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,6 +1,5 @@
 #include "lists.h"
 #include <stdio.h>
-#include <string.h>
 
 /**
  * list_len - returns the number of elements in a linked list_t
@@ -14,29 +13,4 @@ size_t list_len(const list_t *h)
 	for (; h; count_elements++, h = h->next)
 		;
 	return (count_elements);
-}
-
-int main(void)
-{
-	list_t *head;
-	list_t *new;
-	list_t hello = {"World", 5, NULL};
-	size_t n;
-
-	head = &hello;
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	new->str = strdup("Hello");
-	new->len = 5;
-	new->next = head;
-	head = new;
-	n = list_len(head);
-	printf("-> %lu elements\n", n);
-	free(new->str);
-	free(new);
-	return (0);
 }
