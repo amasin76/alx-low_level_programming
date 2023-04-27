a097284e8733d0d82de04181766e766d2493661e
amasin76 <amasin76@gmail.com>
2023-04-27 15:54:03 +0000

    prints a linked list elements

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_list - prints a linked list elements
+ * @h: pointer to the first of the list
+ * Return: number of nodes in the list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count_nodes = 0;
+
+	for (; h; h = h->next, count_nodes++)
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+	return (count_nodes);
+}
