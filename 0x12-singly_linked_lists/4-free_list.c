a5fd136efc24318e2f24e5faaaf2d74bcb94caa4
amasin76 <amasin76@gmail.com>
2023-04-27 17:18:43 +0000

    frees a linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a linked list
+ * @head: pointer to the head of the list
+ */
+void free_list(list_t *head)
+{
+	list_t *current_node;
+
+	while (head)
+	{
+		current_node = head;
+		head = head->next;
+		free(current_node->str);
+		free(current_node);
+	}
+}
