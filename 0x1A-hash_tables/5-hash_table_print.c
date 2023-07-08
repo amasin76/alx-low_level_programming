a803617b7dc9b0ff0be2de2f07f187e105dea920
amasin76 <amasin76@gmail.com>
2023-07-08 21:21:35 +0100

    hash_table_print

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print ordered key:values of hash table
+ * @ht: hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	char *comma = "";
+	hash_node_t *node;
+	unsigned long int idx = 0;
+
+	if (!ht || !ht->array)
+		return;
+
+	putchar('{');
+	while (idx < ht->size)
+	{
+		node = ((ht->array)[idx]);
+		while (node)
+		{
+			printf("%s'%s': '%s'", comma, node->key, node->value);
+			comma = ", ";
+			node = node->next;
+		}
+		idx++;
+	}
+	putchar('}');
+}
