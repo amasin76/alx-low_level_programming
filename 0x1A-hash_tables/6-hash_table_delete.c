afe70a76f10327ef2b6c1821ab7ad732fe52f935
amasin76 <amasin76@gmail.com>
2023-07-08 21:23:04 +0100

    hash_table_delete

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *temp = NULL, *next = NULL;
+	unsigned long int i = 0;
+
+	if (!ht)
+		return;
+
+	for (; i < ht->size; ++i)
+	{
+		temp = ht->array[i];
+		while (temp)
+		{
+			next = temp->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+			temp = next;
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
