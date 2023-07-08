3a11fbd75986021f732ba6553f2c6691a28a8dc4
amasin76 <amasin76@gmail.com>
2023-07-08 21:02:09 +0100

    hash_djb2

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - hash function implementing the djb2 algorithm
+ * @str: string to hash
+ * Return: hash value
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	int c;
+	unsigned long int hash = 5381;
+
+	while ((c = *str++))
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+	return (hash);
+}
